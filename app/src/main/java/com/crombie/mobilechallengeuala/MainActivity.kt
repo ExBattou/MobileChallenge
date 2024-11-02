8a7c9c0eb7287d105863412cb67298d58d1d7347
Adrian A. Narducci <adrian.narducci@gmail.com>
2024-11-02 16:25:28 -0300

    Few Aestethic Changes.

--- app/src/main/java/com/crombie/mobilechallengeuala/MainActivity.kt
+++ app/src/main/java/com/crombie/mobilechallengeuala/MainActivity.kt
@@ -8,22 +8,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -32,9 +35,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -50,17 +55,15 @@ import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.net.URL
-import kotlin.io.path.exists
-import kotlin.io.path.readText
-import kotlin.io.path.writeText
 
-val  BASE_URL = "https://gist.githubusercontent.com/hernan-uala/dce8843a8edbe0b0018b32e137bc2b3a/raw/0996accf70cb0ca0e16f9a99e0ee185fafca7af1/cities.json"
+
+val BASE_URL =
+    "https://gist.githubusercontent.com/hernan-uala/dce8843a8edbe0b0018b32e137bc2b3a/raw/0996accf70cb0ca0e16f9a99e0ee185fafca7af1/cities.json"
 
 
 class MainActivity : ComponentActivity() {
@@ -72,7 +75,7 @@ class MainActivity : ComponentActivity() {
             MobileChallengeUALATheme {
                 val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyNavHost(navController)
+                    AppNavHost(navController)
                 }
             }
         }
@@ -81,9 +84,8 @@ class MainActivity : ComponentActivity() {
 
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyNavHost(navHostController: NavHostController) {
+fun AppNavHost(navHostController: NavHostController) {
     NavHost(navController = navHostController, startDestination = "home") {
         composable("home") { CityListView(navHostController) }
         composable(route = "map/{lat}/{long}",
@@ -103,6 +105,7 @@ fun MyNavHost(navHostController: NavHostController) {
                         GoogleMap(lat, long)
                     }
                 }
+
                 Configuration.ORIENTATION_PORTRAIT -> {
                     Column {
                         BackButton { navHostController.popBackStack() }
@@ -125,7 +128,7 @@ fun CityListView(navHostController: NavHostController) {
     LaunchedEffect(Unit) {
         coroutineScope.launch {
             try {
-                if (cityList.isEmpty()){
+                if (cityList.isEmpty()) {
                     cityList.addAll(fetchCitiesFromGist("https://gist.githubusercontent.com/hernan-uala/dce8843a8edbe0b0018b32e137bc2b3a/raw/0996accf70cb0ca0e16f9a99e0ee185fafca7af1/cities.json"))
                 }
             } finally {
@@ -134,25 +137,15 @@ fun CityListView(navHostController: NavHostController) {
         }
     }
 
-    if (isLoading) {
-        CircularProgressIndicator() // Show loading indicator
-    } else {
-        CityList(cityList,navHostController)
-    }
-}
-
-@Composable
-fun checkOrientation(cityList: List<City>, navHostController: NavHostController) {
-    val configuration = LocalConfiguration.current
-    when (configuration.orientation) {
-        Configuration.ORIENTATION_LANDSCAPE -> {
-            Row {
-                CityList(cityList,navHostController)
-                GoogleMap()
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
+        if (isLoading) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center)
+            {
+                CircularProgressIndicator()
             }
-        }
-        Configuration.ORIENTATION_PORTRAIT -> {
-            CityList(cityList,navHostController)
+
+        } else {
+            CityList(cityList, navHostController)
         }
     }
 }
@@ -169,7 +162,8 @@ fun CityListPreview() {
             coroutineScope.launch {
                 try {
                     cityList.addAll(
-                        fetchCitiesFromGist(BASE_URL))
+                        fetchCitiesFromGist(BASE_URL)
+                    )
                 } finally {
                     isLoading = false
                 }
@@ -177,9 +171,9 @@ fun CityListPreview() {
         }
 
         if (isLoading) {
-            CircularProgressIndicator() // Show loading indicator
+            CircularProgressIndicator(modifier = Modifier.padding(160.dp))
         } else {
-            CityList(cityList,navController = rememberNavController())
+            CityList(cityList, navController = rememberNavController())
         }
     }
 }
@@ -187,8 +181,13 @@ fun CityListPreview() {
 @Composable
 fun CityList(cities: List<City>, navController: NavHostController) {
     var searchQuery by remember { mutableStateOf("") }
-    Column {
-        TextField(
+    Column (modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()) {
+        OutlinedTextField(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
             value = searchQuery,
             onValueChange = { searchQuery = it },
             label = { Text("Search") }
@@ -198,7 +197,7 @@ fun CityList(cities: List<City>, navController: NavHostController) {
         } else {
             cities
         }
-        LazyColumn {
+        LazyColumn (contentPadding = PaddingValues(vertical = 15.dp)){
             items(filteredCities) { city ->
                 CityRow(city) { selectedCity ->
                     navController.navigate("map/${selectedCity.coord.lat}/${selectedCity.coord.lon}")
@@ -230,27 +229,28 @@ suspend fun fetchCitiesFromGist(urlString: String): List<City> = withContext(Dis
     }
 }
 
-suspend fun fetchCitiesFromGist(context: Context, urlString: String): List<City> = withContext(Dispatchers.IO) {
-    val file = File(context.filesDir, "cities.json") // Create a file in internal storage
+suspend fun fetchCitiesFromGist(context: Context, urlString: String): List<City> =
+    withContext(Dispatchers.IO) {
+        val file = File(context.filesDir, "cities.json") // Create a file in internal storage
 
-    if (file.exists()) {
-        // Read from local file if it exists
-        val jsonString = file.readText()
-        val gson = Gson()
-        return@withContext gson.fromJson(jsonString, Array<City>::class.java).toList()
-    } else {
-        // Fetch from Gist and save to local file
-        try {
-            val response = URL(urlString).readText()
-            file.writeText(response) // Save to local file
+        if (file.exists()) {
+            // Read from local file if it exists
+            val jsonString = file.readText()
             val gson = Gson()
-            return@withContext gson.fromJson(response, Array<City>::class.java).toList()
-        } catch (e: Exception) {
-            Log.e("FetchCities", "Error fetching or parsing cities", e)
-            return@withContext emptyList()
+            return@withContext gson.fromJson(jsonString, Array<City>::class.java).toList()
+        } else {
+            // Fetch from Gist and save to local file
+            try {
+                val response = URL(urlString).readText()
+                file.writeText(response) // Save to local file
+                val gson = Gson()
+                return@withContext gson.fromJson(response, Array<City>::class.java).toList()
+            } catch (e: Exception) {
+                Log.e("FetchCities", "Error fetching or parsing cities", e)
+                return@withContext emptyList()
+            }
         }
     }
-}
 
 @Composable
 fun GoogleMap(lat: Double = 39.934444, lng: Double = 43.600555) {
@@ -260,19 +260,17 @@ fun GoogleMap(lat: Double = 39.934444, lng: Double = 43.600555) {
         position = CameraPosition.fromLatLngZoom(where, 10f)
     }
 
-        GoogleMap(
-            modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState
-        ) {
-            Marker(
-                state = whereMarketState   ,
-                title = "Singapore",
-                snippet = "Marker in Singapore"
-            )
-        }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = whereMarketState,
+            title = "Singapore",
+            snippet = "Marker in Singapore"
+        )
     }
-
-
+}
 
 
 @Composable
